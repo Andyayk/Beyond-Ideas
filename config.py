3b5220400717a807bb60e349f70c64114666ba6e
Xu Ying <xuying2319@gmail.com>
2018-10-11 16:52:12 +0800

    change back to mysql

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'
 
 # The SQLAlchemy connection string.
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/app'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/app'
 #SQLALCHEMY_DATABASE_URI = 'postgresql://root:password@localhost/myapp'
 
 # Flask-WTF flag for CSRF
