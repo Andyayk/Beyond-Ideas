d4f68f04221f0e44e27ad5f8f026d983d6ee3bc6
TAN Li Kai <likai.tan.2016@sis.smu.edu.sg>
2019-01-30 18:31:06 +0800

    -Allowed weather crawling's input parameters for more than 1 year -Added Asia Pacific Region

--- static/js/WebCrawlingbi.js
+++ static/js/WebCrawlingbi.js
@@ -12,12 +12,9 @@ class WebCrawlingbi extends Component {
          message: "",
          startdate: "",
          enddate: "",
-         countryname: "",
-         errordatestatement: "",
+         countryname: "",         
       };
 
-      this.validateDateRange = this.validateDateRange.bind(this);      
-      this.enablesubmitbutton = this.enablesubmitbutton.bind(this);
       this.selectStartDate = this.selectStartDate.bind(this); 
       this.selectEndDate = this.selectEndDate.bind(this);
 
@@ -27,39 +24,9 @@ class WebCrawlingbi extends Component {
 
       this.formSubmitted = this.formSubmitted.bind(this);      
    }
-   
-   validateDateRange(fromDate, toDate){
-        if(fromDate && toDate && fromDate > toDate){
-            this.setState({
-                errordatestatement: "Please select a valid date range"
-            });
-            this.enablesubmitbutton(false);
-        }else {
-            this.setState({errordatestatement: ""});
-            this.enablesubmitbutton(true);
-        }
-   }
-   
-   enablesubmitbutton(enable){
-       if(enable){
-            var element = document.getElementById('submitbutton');
-            element.disabled = false;
-            element.style.background = "#4CAF50";
-            element.style.opacity = "1";            
-            element.style.cursor = "pointer";
-       } else {
-            var element = document.getElementById('submitbutton');
-            element.disabled = true;
-            element.style.background = "red";
-            element.style.opacity = "0.6";
-            element.style.cursor = "not-allowed";
-       }
-   }
-   
 
    //store the start date values that the user has selected
    selectStartDate(event) {
-      this.validateDateRange(event.target.value,this.state.enddate);
       this.setState({
          startdate: event.target.value
       });      
@@ -67,7 +34,6 @@ class WebCrawlingbi extends Component {
 
    //store the end date values that the user has selected
    selectEndDate(event) {
-      this.validateDateRange(this.state.startdate,event.target.value);
       this.setState({
          enddate: event.target.value
       });      
@@ -82,29 +48,17 @@ class WebCrawlingbi extends Component {
 
    //retrieve web crawl results
    weatherCrawler(event){
-      var country = this.state.countryname
-      var begindate = this.state.startdate
-      var finishdate = this.state.enddate
       $.post(window.location.origin + "/weathercrawlingbi/",
       {
-         startdate: begindate,
-         enddate: finishdate,
-         countryname: country
+         startdate: this.state.startdate,
+         enddate: this.state.enddate,
+         countryname: this.state.countryname
       },
       (data) => {
-        
 
          var message = "";
          $.each(data, function(key, val) {
-            var element = document.createElement('a');
-            var newContent = val.replace(/;/g, "\n")
-            element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(newContent));
-            element.setAttribute('download', 'weather_' + country + '_' + begindate + '_' + finishdate + '.csv');
-            element.style.display = 'none';
-            document.body.appendChild(element);
-            element.click();
-            document.body.removeChild(element);
-            message = "Crawling of weather data successful.";
+            message = val;
          });  
 
          this.setState({
@@ -112,7 +66,6 @@ class WebCrawlingbi extends Component {
          });                        
       });
    }
-
 
    //handle form submission
    formSubmitted(event){
@@ -164,7 +117,6 @@ class WebCrawlingbi extends Component {
                                     <input type="date" style={{"width":"200px"}} min="1900-01-01" max="2100-12-31" required onChange={this.selectEndDate} />
                                  </td>
                               </tr>
-                              <div class="carderrormsg">{this.state.errordatestatement}</div>
                               <br/>
                               <tr>
                                  <td align="center">
@@ -175,14 +127,49 @@ class WebCrawlingbi extends Component {
                               </tr><tr>                             
                                  <td align="center">
                                     <div class="cardsubtitle">
-                                       Country: e.g. Victoria Island
+                                       Country: e.g. Singapore
                                     </div>
                                  </td>
                               </tr><tr>
                                  <td align="center">
                                     <select required onChange={this.selectCountryName} style={{"width":"200px"}}>
                                        <option value="" selected>-------- select a country --------</option>
-                                       <option value="Singapore">Singapore</option>                                          
+                                       <option value="Australia">Australia</option>
+                                       <option value="Bangladesh">Bangladesh</option>
+                                       <option value="Bhutan">Bhutan</option> 
+                                       <option value="Brunei">Brunei</option> 
+                                       <option value="Myanmar">Myanmar</option> 
+                                       <option value="Cambodia">Cambodia</option> 
+                                       <option value="China">China</option> 
+                                       <option value="Chile">Chile</option> 
+                                       <option value="Cook Islands">Cook Islands</option> 
+                                       <option value="Fiji">Fiji</option> 
+                                       <option value="India">India</option> 
+                                       <option value="Indonesia">Indonesia</option> 
+                                       <option value="Japan">Japan</option> 
+                                       <option value="Kiribati">Kiribati</option> 
+                                       <option value="Malaysia">Malaysia</option>  
+                                       <option value="Maldives">Maldives</option>  
+                                       <option value="Marshall Islands">Marshall Islands</option>  
+                                       <option value="Micronesia">Micronesia</option>  
+                                       <option value="Mongolia">Mongolia</option>  
+                                       <option value="Nauru">Nauru</option>  
+                                       <option value="New Zealand">New Zealand</option>  
+                                       <option value="Niue">Niue</option>  
+                                       <option value="Pakistan">Pakistan</option>  
+                                       <option value="Palau">Palau</option>  
+                                       <option value="Papua New Guinea">Papua New Guinea</option>  
+                                       <option value="Peru">Peru</option>
+                                       <option value="Philippines">Philippines</option>    
+                                       <option value="Russia">Russia</option>  
+                                       <option value="Samoa">Samoa</option>
+                                       <option value="Singapore">Singapore</option>
+                                       <option value="Solomon Island">Solomon Island</option>
+                                       <option value="South Korea">South Korea</option>   
+                                       <option value="Sri Lanka">Sri Lanka</option>
+                                       <option value="Tuvalu">Tuvalu</option>   
+                                       <option value="Vanuatu">Vanuatu</option>   
+                                       <option value="Vietnam">Vietnam</option>
                                     </select>
                                  </td>
                               </tr>
